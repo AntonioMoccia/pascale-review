3ff8836da1ab1de394871c429651321b4bb41e85
Antonio Moccia <antonio.moccia@ekd.local>
2023-08-31 11:30:24 +0200

    creata pagina prodotto e sistemato menu mobile

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,33 +1,112 @@
-import React from "react";
-
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { CiMenuBurger } from "react-icons/ci";
 function Navbar() {
+  const [open, setOpen] = useState(false);
+
+
+  useEffect(() => {
+    if (window.innerWidth <= 992) {
+      if (open) {
+        document.querySelector(".nav").style.transform = "translateX(0)";
+        document.querySelector(".overflow").style.display = "block";
+      } else {
+        document.querySelector(".nav").style.transform = "translateX(-100%)";
+        document.querySelector(".overflow").style.display = "none";
+      }
+    } else {
+    }
+  }, [open]);
+
+  const clickAwayEvent = (e) => {
+    if (e.target == document.querySelector(".overflow")) {
+      document.querySelector(".nav").style.transform = "translateX(-100%)";
+      document.querySelector(".overflow").style.display = "none";
+    }
+  };
+  useEffect(() => {
+    document.addEventListener("click", clickAwayEvent);
+    document.addEventListener("touchstart", clickAwayEvent);
+    window.addEventListener('resize',(e)=>{
+      if(e.target.innerWidth <= 992){
+        document.querySelector(".nav").style.transform = "translateX(-100%)";
+        document.querySelector(".overflow").style.display = "none";
+      }else{
+        document.querySelector(".nav").style.transform = "translateX(0)";
+        document.querySelector(".overflow").style.display = "block";
+      }
+    })
+  }, []);
+
   return (
     <header className="header-navbar">
-        <div className="mobile-logo">
-          <img src="./pascale-logo.png" />
-        </div>
-      <nav className="nav">
-        <ul>
-          <li className="item-menu">
-            <a className="item-menu-link">Home</a>
-          </li>
-          <li className="item-menu">
-            <a className="item-menu-link">Chi siamo</a>
-          </li>
-          <li className="item-menu logo">
-            <img src="./pascale-logo.png" />
-          </li>
-          <li className="item-menu">
-            <a className="item-menu-link">Prodotti</a>
-          </li>
-          <li className="item-menu">
-            <a className="item-menu-link">Contatti</a>
-          </li>
-        </ul>
-        <div>
-   {/*        content */}
-        </div>
-      </nav>
+      <div className="mobile-logo">
+        <Link href={"/"}>
+        
+          <img src="/pascale-logo.png" />
+        </Link>
+      </div>
+      <div
+        className="burger"
+        onClick={() => {
+          setOpen(!open);
+        }}
+      >
+        <CiMenuBurger />
+      </div>
+      <div className="overflow">
+        <nav className="nav">
+          <ul>
+            <li className="item-menu">
+              <Link
+                onClick={() => {
+                  setOpen(false);
+                }}
+                href={"/"}
+              >
+                Home
+              </Link>
+            </li>
+            <li className="item-menu">
+              <Link
+                onClick={() => {
+                  setOpen(false);
+                }}
+                href={"/chi-siamo"}
+              >
+                Chi siamo
+              </Link>
+            </li>
+            <li className="item-menu logo">
+              <Link href={"/"}>
+                {" "}
+                <img src="/pascale-logo.png" />
+              </Link>
+            </li>
+            <li className="item-menu">
+              <Link
+                onClick={() => {
+                  setOpen(false);
+                }}
+                href={"/prodotti"}
+              >
+                Prodotti
+              </Link>
+            </li>
+            <li className="item-menu">
+              <Link
+                onClick={() => {
+                  setOpen(false);
+                }}
+                href={"/contatti"}
+              >
+                Contatti
+              </Link>
+            </li>
+          </ul>
+          <div>{/*        content */}</div>
+        </nav>
+      </div>
     </header>
   );
 }
--- pages/prodotti/[prodotto].js
+++ pages/prodotti/[prodotto].js
@@ -1,10 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
 
+import "swiper/css";
+import "swiper/css/navigation";
 
 const Prodotto = (props) => {
   return (
-    <div>Prodotto</div>
-  )
-}
+    <div className="prodotto-container">
+      <div className="titolo-swiper">
+        <h2>Castagne fresche calibrate</h2>
+        <section className="swiper-container">
+          <Swiper
+            modules={[Navigation]}
+            className="swiper"
+            navigation
+            spaceBetween={0}
+            slidesPerView={1}
+            onSlideChange={() => console.log("slide change")}
+            onSwiper={(swiper) => console.log(swiper)}
+          >
+            <SwiperSlide className="swiper-slide">Slide 1</SwiperSlide>
+            <SwiperSlide className="swiper-slide">Slide 2</SwiperSlide>
+            <SwiperSlide className="swiper-slide">Slide 3</SwiperSlide>
+            <SwiperSlide className="swiper-slide">Slide 4</SwiperSlide>
+          </Swiper>
+        </section>
+      </div>
+      <div className="prodotto-descrizione">
+        Laboris pariatur laborum excepteur ad eu aliquip veniam do incididunt.
+        Est occaecat esse quis proident voluptate commodo quis. Ex velit minim
+        cillum proident reprehenderit sunt enim. Tempor ad ut sit duis aliquip
+        sunt ea est in. Ut officia dolore mollit ullamco qui laborum anim velit
+        ex consequat qui exercitation. Sunt ipsum consectetur exercitation amet
+        minim aliqua quis ex. Et et magna ea qui cillum Lorem nulla. Occaecat
+        amet aliqua nostrud amet consequat pariatur labore tempor deserunt quis.
+        Sit ipsum exercitation dolore culpa veniam duis cupidatat elit ad.
+        Eiusmod pariatur in voluptate sunt incididunt sit eu cillum. Minim est
+        exercitation occaecat aliqua sit.
+      </div>
+    </div>
+  );
+};
 
-export default Prodotto
+export default Prodotto;
